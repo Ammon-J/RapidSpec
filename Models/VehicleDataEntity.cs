8ecf6f5a197f3bb012b3722e39332f1cb0fb4e78
ammon <ammonsawesome@gmail.com>
2025-09-24 15:57:47 -0600

    Add project files.

---
+++ Models/VehicleDataEntity.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RapidSpec.Models
+{
+    public class VehicleDataEntity
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public string EngineName { get; set; }
+        public string EngineType { get; set; }
+        public decimal Price { get; set; }
+    }
+}
